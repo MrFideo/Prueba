da78c615c077af3afeb8873821544738d3cd0c06
David <davidaccionpirrata@gmail.com>
2024-08-20 19:53:05 -0600

    David Mendez 20/08/2024
    Primer commit

---
+++ app/src/main/java/com/tecmilenio/myapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.tecmilenio.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etNombre;
+    private EditText etEscuela;
+    private EditText etCarrera;
+    private EditText etMateria;
+    private Button borrar;
+    private Button guardar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etNombre=(EditText) findViewById(R.id.etNombre);
+        etEscuela=(EditText) findViewById(R.id.etEscuela);
+        etCarrera=(EditText) findViewById(R.id.etCarrera);
+        etMateria=(EditText) findViewById(R.id.etMateria);
+
+        borrar=(Button) findViewById(R.id.borrar);
+        guardar=(Button) findViewById(R.id.guardar);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+
+                String nombre = etNombre.getText().toString();
+                String escuela = etEscuela.getText().toString();
+                String carrera = etCarrera.getText().toString();
+                String materia = etMateria.getText().toString();
+
+                System.out.println("El nombre es: "+nombre+"\nLa escuela es: "+escuela+"\nLa carrera es: "+carrera+"\nLa materia es: "+materia);
+            }
+        });
+
+
+    }
+}
